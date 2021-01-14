22de22a7653e9ed0369df0238eb7742592f55986
Cosnavel <coding@niclaskahlmeier.de>
2021-01-14 16:48:22 +0100

    wip

--- public/script.js
+++ public/script.js
@@ -1,10 +1,90 @@
-console.log(Array.from(document.querySelectorAll("blockcode")).filter(elem => elem.attributes.src === undefined))
+const blockcodesInQuestionText = Array.from(
+	document.querySelectorAll('question_text > blockcode'),
+).filter(elem => !elem.hasAttribute('src'))
+const blockcodesInAnswer = Array.from(
+	document.querySelectorAll('answer > blockcode'),
+).filter(elem => !elem.hasAttribute('src'))
+const elements = Array.from(document.querySelectorAll('question'))
 
-
-fetch('http://127.0.0.1:8081/test', {
-  method: 'POST', // or ‘PUT’
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({"data": "test"}),
+//alle blockcodes in questions
+let i = 1
+elements.forEach(element => {
+	question_text_bcs = []
+	answer_bcs = []
+	blockcodesInQuestionText.forEach(bc => {
+		if (element.contains(bc)) {
+			question_text_bcs.push(bc.innerText)
+		}
+	})
+	blockcodesInAnswer.forEach(bc => {
+		if (element.contains(bc)) {
+			answer_bcs.push(bc.innerText)
+		}
+	})
+	if ((question_text_bcs.length > 0) | (answer_bcs.length > 0)) {
+		console.log({
+			id: i,
+			question_text_bcs,
+			answer_bcs,
+		})
+		i++
+	}
 })
+
+const blockcodes = Array.from(document.querySelectorAll('blockcode')).filter(
+	elem => !elem.hasAttribute('src'),
+)
+elements.forEach(element => {
+	let y = 1
+	blockcodes.forEach(bc => {
+		if (!element.contains(bc)) {
+			let data = {
+				id: y,
+				value: bc.innerText,
+			}
+			y++
+			//anfrage with data
+			if (y < 3) {
+				//mutate element
+				console.log('------------')
+				console.log('Before')
+				console.log(bc)
+				bc.innerText = ''
+				bc.setAttribute('src', `code/${y}.js`)
+				console.log('After')
+				console.log(bc)
+				console.log('-------------')
+			}
+		}
+	})
+})
+
+// const elements = Array.from(document.querySelectorAll('blockcode')).filter(
+// 	elem => !elem.hasAttribute('src'),
+// )
+//.filter(elem => console.log(elem.parentElement !== h1))
+
+//blockcode with question?text parent
+// elements.forEach(el => {
+// 	console.log(el.closest('question_text > blockcode'))
+// })
+//ANswer Parent
+// elements.forEach(el => {
+// 	console.log(el.closest('answer blockcode'))
+// })
+// console.log(
+// 	Array.from(document.querySelectorAll('answer < blockcode')).filter(
+// 		elem => elem.attributes.src === undefined,
+// 	),
+// )
+
+let data = {
+	name: 'Georg',
+}
+// fetch('http://127.0.0.1:8081/test', {
+// 	method: 'POST', // or ‘PUT’
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 	},
+// 	body: JSON.stringify(data),
+// })
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const fs = require('fs')
-const path = require('path');
+const path = require('path')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const ip = '127.0.0.1'
 const port = '8081'
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'))
 
-app.get('/test', (res, req) => {
-    console.log(req.body)
+app.post('/test', (req, res) => {
+	console.log(req.body)
+	console.log(req.body.name)
+	console.log(req.body.kinder)
 })
 
 app.listen(port, ip, () => {
-    console.log(`Server running at http://${ip}:${port}/`)
+	console.log(`Server running at http://${ip}:${port}/`)
 })
