93e345f79d0b70073e3204241ba8527879e65ab6
Cosnavel <coding@niclaskahlmeier.de>
2021-01-20 08:43:56 +0100

    wip

--- tmp.js
+++ tmp.js
@@ -18,27 +18,26 @@ const removeEntities = (entities, source) => {
 file = removeEntities(
 	{
 		'&nbsp;': '&#x00A0;',
-		'&nbsp;': '&#x00A0;',
 		'&shy;': '&#x00AD;',
-		'&thinsp;': '&#x202F;',
-		'&zwsp;': '&#x200B;',
-		'&ndash;': '&#8211;',
-		'&mdash;': '&#x2014;',
-		'&hellip;': '&#8230;',
-		'&times;': '&#215;',
-		'&darr;': '&#8595;',
-		'&larr;': '&#8592;',
-		'&uarr;': '&#8593;',
-		'&rarr;': '&#8594;',
-		'&rsquo;': '&#8217;',
-		'&lsquo;': '&#8216;',
-		'&raquo;': '&#187;',
-		'&laquo;': '&#171;',
-		'&rsaquo;': '&#8250;',
-		'&lsaquo;': '&#8249;',
-		'&bdquo;': '&#8222;',
-		'&ldquo;': '&#8220;',
-		'&rdquo;': '&#8221;',
+		'&thinsp;': '',
+		'&zwsp;': '',
+		'&ndash;': '–',
+		'&mdash;': '—',
+		'&hellip;': '…',
+		'&times;': '×',
+		'&darr;': '↓',
+		'&larr;': '←',
+		'&uarr;': '↑',
+		'&rarr;': '→',
+		'&rsquo;': '’',
+		'&lsquo;': '‘',
+		'&raquo;': '»',
+		'&laquo;': '«',
+		'&rsaquo;': '›',
+		'&lsaquo;': '‹',
+		'&bdquo;': '„',
+		'&ldquo;': '“',
+		'&rdquo;': '”',
 		'&table_of_content_solution;':
 			'https://github.com/Webmasters-Europe/js_dom_table_of_content/releases/tag/2.7.0',
 		'&search_and_replace_solution;':
@@ -235,3 +234,53 @@ refactorBlockCodeOutsideOfQuestions(blockcodes, questions)
 refactorBlockCodeInsideOfQuestions()
 
 exportHTML()
+
+// let newBookXML = fs.readFileSync('./dist/html/book.xml', 'utf8')
+// newBookXML = removeEntities(
+// 	{
+// 		// '&#x00A0;': '&nbsp;',
+// 		// '&#x00A0;': '&nbsp;',
+// 		// '&#x00AD;': '&shy;',
+// 		// '&#x202F;': '&thinsp;',
+// 		// '&#x200B;': '&zwsp;',
+// 		// '&#8211;': '&ndash;',
+// 		// '&#x2014;': '&mdash;',
+// 		// '&#8230;': '&hellip;',
+// 		// '&#215;': '&times;',
+// 		// '&#8595;': '&darr;',
+// 		// '&#8592;': '&larr;',
+// 		// '&#8593;': '&uarr;',
+// 		// '&#8594;': '&rarr;',
+// 		// '&#8217;': '&rsquo;',
+// 		// '&#8216;': '&lsquo;',
+// 		// '&#187;': '&raquo;',
+// 		// '&#171;': '&laquo;',
+// 		// '&#8250;': '&rsaquo;',
+// 		// '&#8249;': '&lsaquo;',
+// 		// '&#8222;': '&bdquo;',
+// 		// '&#8220;': '&ldquo;',
+// 		// '&#8221;': '&rdquo;',
+
+// 		'https://github.com/Webmasters-Europe/js_dom_table_of_content/releases/tag/2.7.0':
+// 			'&table_of_content_solution;',
+
+// 		'https://github.com/Webmasters-Europe/js_dom_search_and_replace/releases/tag/2.7.0':
+// 			'&search_and_replace_solution;',
+// 		'https://tableofcontent.webmasters-akademie.dev/': '&toc_demo;',
+// 		'https://search-replace.webmasters-akademie.dev/': '&sar_demo;',
+
+// 		'https://github.com/Webmasters-Europe/js_basics_royal_flush/releases/tag/1.9.0':
+// 			'&royal_flush_solution;',
+
+// 		'https://github.com/Webmasters-Europe/js_basics_flood_fill/releases/tag/1.9.0':
+// 			'&flood_fill_solution;',
+
+// 		'https://github.com/Webmasters-Europe/js_basics_alk_test/releases/tag/1.9.0':
+// 			'&alk_test_solution;',
+
+// 		'https://terminal.webmasters-akademie.dev': '&terminal_demo;',
+// 	},
+// 	newBookXML,
+// )
+
+// fs.writeFileSync('./dist/html/book.xml', newBookXML)
