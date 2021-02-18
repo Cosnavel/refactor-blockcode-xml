246275a37c7a964f5f1374029e29c6b06efdd66d
Cosnavel <niclasdevelop@gmail.com>
2021-02-18 15:03:25 +0000

    (CI) format code

--- src/broken-link-checker/lib/internal/parseHtml.js
+++ src/broken-link-checker/lib/internal/parseHtml.js
@@ -1,25 +1,21 @@
-"use strict";
-var isStream = require("is-stream");
-var isString = require("is-string");
-var parse5 = require("parse5");
+'use strict'
+var isStream = require('is-stream')
+var isString = require('is-string')
+var parse5 = require('parse5')
 
-var treeAdapter = Object.create( parse5.treeAdapters.default );
-treeAdapter.createElement_old = treeAdapter.createElement;
-treeAdapter.createElement = function(tagName, namespaceURI, attrs)
-{
-	var result = treeAdapter.createElement_old(tagName, namespaceURI, attrs);
-	
-	if (result.attrs != null)
-	{
-		result.attrMap = getAttrMap(result.attrs);
-	}
-	
-    return result;
-};
+var treeAdapter = Object.create(parse5.treeAdapters.default)
+treeAdapter.createElement_old = treeAdapter.createElement
+treeAdapter.createElement = function (tagName, namespaceURI, attrs) {
+    var result = treeAdapter.createElement_old(tagName, namespaceURI, attrs)
 
-var options = { locationInfo:true, treeAdapter:treeAdapter };
+    if (result.attrs != null) {
+        result.attrMap = getAttrMap(result.attrs)
+    }
 
+    return result
+}
 
+var options = { locationInfo: true, treeAdapter: treeAdapter }
 
 /*
 	Convert attributes array to a map.
@@ -27,51 +23,37 @@ var options = { locationInfo:true, treeAdapter:treeAdapter };
 	Note: parse5 will have already handled multiple attrs of the
 	same name.
 */
-function getAttrMap(attrs)
-{
-	var i;
-	var map = {};
-	var numAttrs = attrs.length;
-	
-	for (i=0; i<numAttrs; i++)
-	{
-		map[ attrs[i].name ] = attrs[i].value;
-	}
-	
-	return map;
+function getAttrMap(attrs) {
+    var i
+    var map = {}
+    var numAttrs = attrs.length
+
+    for (i = 0; i < numAttrs; i++) {
+        map[attrs[i].name] = attrs[i].value
+    }
+
+    return map
 }
-
-
 
 /*
 	Parse an HTML stream/string and return a tree.
 */
-function parseHtml(input)
-{
-	return new Promise( function(resolve, reject)
-	{
-		if (isStream(input) === true)
-		{
-			var parser = new parse5.ParserStream(options);
-			
-			parser.on("finish", function()
-			{
-				resolve(parser.document);
-			});
-			
-			input.pipe(parser);
-		}
-		else if (isString(input) === true)
-		{
-			resolve( parse5.parse(input, options) );
-		}
-		else
-		{
-			reject("Invalid input");
-		}
-	});
+function parseHtml(input) {
+    return new Promise(function (resolve, reject) {
+        if (isStream(input) === true) {
+            var parser = new parse5.ParserStream(options)
+
+            parser.on('finish', function () {
+                resolve(parser.document)
+            })
+
+            input.pipe(parser)
+        } else if (isString(input) === true) {
+            resolve(parse5.parse(input, options))
+        } else {
+            reject('Invalid input')
+        }
+    })
 }
 
-
-
-module.exports = parseHtml;
+module.exports = parseHtml
